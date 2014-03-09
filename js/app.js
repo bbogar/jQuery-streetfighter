c317800ed9c70b510aa27d0513f23adcf9ee91c8
bbogar <blaine.bogar@gmail.com>
2014-03-08 22:19:20 -0700

    Fixed duplicate keydown event bug

--- js/app.js
+++ js/app.js
@@ -24,13 +24,14 @@ $(document).ready(function(){
 
 	$(document).on("keydown", function(x){
 		if(x.keyCode == 88) {
+			$(".ryu-still").hide();
 			$(".ryu-ready").hide();
 			$(".ryu-cool").show();
 		}
 	})
 	.on("keyup", function(x){
 		if(x.keyCode == 88) {
-			$(".ryu-ready").show();
+			$(".ryu-still").show();
 			$(".ryu-cool").hide();
 		}
 	});
